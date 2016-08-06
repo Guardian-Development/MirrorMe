9275ad739ef71e8bc72d07b3b691d26b251d2709
Joe Honour <joehonour@hotmail.co.uk>
2016-08-06 16:30:33 +0000

    Allow user to see only name of the wifi network to mirror.

--- wirelessScanner.py
+++ wirelessScanner.py
@@ -1,12 +1,28 @@
 import subprocess as sub
 import sys
+import re
 
 #prints all the information about the available wireless networks out. 
-def searchWirlessNetworks():
+def searchWirelessNetworks():
+
+    #make Linux command call to list WiFi networks. 
     out = sub.Popen(['sudo', 'iwlist', 'wlan0', 'scan' ],stdout=sub.PIPE, stderr=sub.PIPE)
     output, errors = out.communicate()
-    print output
+
+    #search output using REGEX to find ESSID names to show to user. 
+    pattern = r'\bESSID\b\:\"[^.]*\"'
+    regex = re.compile(pattern, re.IGNORECASE)
+    for match in regex.finditer(output):
+        print match.group(0)
+    print("complete search") 
 
 #ENTRY POINT
 if __name__ == "__main__":
-   searchWirlessNetworks()
+
+    #NO OP
+    if len(sys.argv) < 2:
+        print("Please specify a command")
+        
+    #SEARCH OPERATION SPECIFIED
+    if sys.argv[1] == "search":
+        searchWirelessNetworks()
